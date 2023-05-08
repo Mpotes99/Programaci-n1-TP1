ced94f2c5c31a16676ccaf80e9814e0e6059dabb
Mauricio Potes <potesmauricio99@gmail.com>
2023-05-07 21:24:59 -0300

    el 2

--- Parte2/js2.js
+++ Parte2/js2.js
@@ -1,6 +1,20 @@
 'use strict' ; 
 
-let productos = [] 
+function cargarProducto() {
+    let codigo = prompt("Ingrese el código del producto:");
+    let nombre = prompt("Ingrese el nombre del producto:");
+    let categoria = prompt("Ingrese la categoría del producto:");
+    let precio = prompt("Ingrese el precio del producto:");
+    let descripcion = prompt("Ingrese la descripción del producto:");
 
+    let producto = {
+        codigo: codigo,
+        nombre: nombre,
+        categoria: categoria,
+        precio: precio,
+        descripcion: descripcion,
+};
 
-
+    productos.push(producto);
+    alert("Producto cargado correctamente.");
+}
